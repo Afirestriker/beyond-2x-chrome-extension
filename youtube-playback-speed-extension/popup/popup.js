2f6319eecfb40ac9cd7455b64a18830e2111ae5b
Afirestriker <agrawalashish.in@gmail.com>
2025-03-15 14:50:26 +0530

    feat: add and update popup page

--- youtube-playback-speed-extension/popup/popup.js
+++ youtube-playback-speed-extension/popup/popup.js
@@ -1,1 +1,6 @@
-console.log("Playback extension loaded");
+const slider = document.getElementById("beyond-2x-youtube-range-slider");
+const output = document.getElementById("beyond-2x-youtube-range-slider-value");
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
